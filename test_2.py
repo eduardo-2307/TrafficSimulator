9b2f0abe448b3ca98e2f49221257b7a62b7b5772
Eduardo Maldonado <a01562053@itesm.mx>
2021-12-03 12:22:38 -0700

    correcciones 3

--- test_2.py
+++ test_2.py
@@ -12,39 +12,39 @@ port = int(os.getenv('PORT', 8000))
 def root():
     return jsonify([{"message":"Pruebas Tec, from IBM Cloud!"}])
 
-# sim = Simulation()
+sim = Simulation()
 
-# sim.create_roads([
-#     ((0, 100), (148, 100)),
-#     ((148, 100), (300, 100)),
+sim.create_roads([
+    ((0, 100), (148, 100)),
+    ((148, 100), (300, 100)),
 
-#     ((300, 95), (158, 95)),
-#     ((158, 95), (0, 95)),
+    ((300, 95), (158, 95)),
+    ((158, 95), (0, 95)),
     
-#     ((150, 0), (150, 92)),
-#     ((150, 92), (150, 200)),
+    ((150, 0), (150, 92)),
+    ((150, 92), (150, 200)),
 
-#     ((155, 200), (155, 102)),
-#     ((155, 102), (155, 0))
-# ])
+    ((155, 200), (155, 102)),
+    ((155, 102), (155, 0))
+])
 
-# sim.create_gen({
-#     'vehicle_rate': 10,
-#     'vehicles': [
-#         [1, {"path": [0, 1]}],
-#         [1, {"path": [2, 3]}],
-#         [1, {"path": [4, 5]}],
-#         [1, {"path": [6, 7]}]
-#     ]
-# })
+sim.create_gen({
+    'vehicle_rate': 10,
+    'vehicles': [
+        [1, {"path": [0, 1]}],
+        [1, {"path": [2, 3]}],
+        [1, {"path": [4, 5]}],
+        [1, {"path": [6, 7]}]
+    ]
+})
 
-# sim.create_signal([[0], [4]])
-# sim.create_signal([[2], [6]])
+sim.create_signal([[0], [4]])
+sim.create_signal([[2], [6]])
 
 
-# win = Window(sim)
-# win.offset = (-150, -110)
-# win.run(steps_per_update=10)
+win = Window(sim)
+win.offset = (-150, -110)
+win.run(steps_per_update=10)
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=port, debug=True)
